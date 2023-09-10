6c75b24aead062427fe22ef5cc7c78967de5cc4d
SoonHyung_Lee <tnsgud9@naver.com>
2023-09-11 02:26:57 +0900

    ADD : Add package Tailwind CSS + DaisyUI
    [디자인 관련 라이브러리 추가]
    * Tailwind CSS, DaisyUI 패키지 추가
    * Styled Component 추가
    * react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import daisyuiLogo from './assets/daisyui.png';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://daisyui.com/" target="_blank" rel="noreferrer">
-          <img src={daisyuiLogo} className="logo daisyui" alt="Daisyui logo" />
-        </a>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <input
+            type="button"
+            className="btn btn-primary"
+            value="Get Started"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <input
-          type="button"
-          onClick={() => setCount((count) => count + 1)}
-          value={`count is ${count}`}
-        />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
